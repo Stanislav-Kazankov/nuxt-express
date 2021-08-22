0f1978e095dbc3821c46b475d319a65c60186206
Stanislav-Kazankov <s.v.kazankov@mail.ru>
2021-08-22 18:48:46 +0900

    Выполняет практическое задание из карточки 'Принципы работы бэкенда'

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <section>
+      {{ message }}
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    const response = await $axios.get('/')
+    return { message: response.data }
+  }
+}
+</script>
